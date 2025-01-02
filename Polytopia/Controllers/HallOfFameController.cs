33bd662502bb2e47c08968b2728ab778c9ae881e
Fabian Bernat <fabbernat@gmail.com>
2025-01-03 00:04:49 +0100

    HallOfFame lists 100 players, all white, no icons

---
+++ Polytopia/Controllers/HallOfFameController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Polytopia.Models;
+
+namespace Polytopia.Controllers
+{
+    public class HallOfFameController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Mock data for demonstration
+            var highScores = new List<HighScoreEntry>
+        {
+            new HighScoreEntry { Username = "Midjiwan", Score = 252578 },
+            new HighScoreEntry { Username = "Zoy", Score = 195069 },
+            new HighScoreEntry { Username = "Innofunni", Score = 195069 },
+            new HighScoreEntry { Username = "GullYY", Score = 172330 }
+        };
+
+            // Pass the list to the view
+            return View(highScores);
+        }
+    }
+}
---
+++ Polytopia/Models/HighScoreEntry.cs
@@ -0,0 +1,9 @@
+﻿namespace Polytopia.Models
+{
+    public class HighScoreEntry
+    {
+        public string Username { get; set; }
+        public int Score { get; set; }
+    }
+
+}
